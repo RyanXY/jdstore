c28c8b3ba03e08b3de29ed69b44022b438b4c43d
ryanxueying <xueying.ryan@gmail.com>
2017-03-10 13:57:08 +0100

    implement image upload

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,8 @@ class Product < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :quantity, presence: true
-  validates :price, presence: true 
+  validates :price, presence: true
+
+  mount_uploader :image, ImageUploader
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309222734) do
+ActiveRecord::Schema.define(version: 20170310123853) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170309222734) do
     t.integer  "quantity"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
