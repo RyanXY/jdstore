d525b1c292de264676b2316475d14d44445a0e5c
ryanxueying <xueying.ryan@gmail.com>
2017-03-15 09:23:06 +0100

    add order controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,18 @@
+class OrdersController < ApplicationController
+
+def create
+  @order = Order.new(order_params)
+  @order.user = current_user
+  @order.total = current_cart.total_price
+
+  if @order.save
+    redirect_to order_path(@order)
+
+  else
+    render "carts/checkout"
+  end
+
+end
+
+
+end
